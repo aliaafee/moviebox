2c5126d8e86a44dafb56dfd5351471897535f161
Ali Aafee <ali>
2011-08-14 08:49:07 +0000

    Fixed path names
    git-svn-id: file:///Users/ali/SvnRepo/moviebox/movie-organizer/trunk@6 da9b8d34-38d1-418a-a872-973fd07dfd50

--- movie-organizer-win/package.bat
+++ movie-organizer-win/package.bat
@@ -4,7 +4,6 @@ mkdir install\movie-organizer
 copy src\dist\*.* install\movie-organizer
 mkdir install\movie-organizer\res
 copy src\dist\res\*.* install\movie-organizer\res\
-copy progs\Autorun.inf install\
 copy progs\movie-organizer.exe install\
 copy progs\README.txt install\
 xcopy /S progs\VLC install\VLC\

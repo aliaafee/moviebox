72dbb6ed2ac10eda73566239fed2327470c30334
Ali Aafee <ali.aafee@gmail.com>
2011-08-21 22:45:00 +0000

    fixed license
    git-svn-id: file:///Users/ali/SvnRepo/moviebox/trunk@52 da9b8d34-38d1-418a-a872-973fd07dfd50

--- movie-organizer/WebCatch.py
+++ movie-organizer/WebCatch.py
@@ -1,25 +1,23 @@
-"""
-	portable-movie-organizer
-	------------------------
-	
-	Copyright (C) 2010 Ali Aafee
-	
-	This file is part of portable-movie-organizer.
-
-	portable-movie-organizer is free software: you can redistribute it 
-	and/or modify it under the terms of the GNU General Public License 
-	as published by the Free Software Foundation, either version 3 of 
-	the License, or (at your option) any later version.
-
-	portable-movie-organizer is distributed in the hope that it will 
-	be useful, but WITHOUT ANY WARRANTY; without even the implied 
-	warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
-	See the GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with portable-movie-organizer.  
-	If not, see <http://www.gnu.org/licenses/>.
-"""
+#
+#	portable-movie-organizer
+#	
+#	Copyright (c) 2010 Ali Aafee
+#	
+#	This file is part of portable-movie-organizer.
+#
+#	portable-movie-organizer is free software: you can redistribute it 
+#	and/or modify it under the terms of the GNU General Public License 
+#	as published by the Free Software Foundation, either version 3 of 
+#	the License, or (at your option) any later version.
+#
+#	portable-movie-organizer is distributed in the hope that it will 
+#	be useful, but WITHOUT ANY WARRANTY; without even the implied 
+#	warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+#	See the GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with portable-movie-organizer.  
+#	If not, see <http://www.gnu.org/licenses/>.
 
 import sqlite3
 import urllib
